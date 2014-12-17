2e1f00907e4fab5c4e40d63d7251dd3315b82772
Austin T. Conn <atc3030dev@gmail.com>
2014-12-17 13:28:00 -0500

    Settings: Forward Port Volume Long Press Skip Tracks (1/2)
    Bring Forward Volume Rocker Track Change
    
    Requires Settings Change
    http://review.cyanogenmod.org/#/c/82222/
    
    Change-Id: I70b00e08fe48707110725dd1daa56b1e48d15a03
    
    Fix volume key music controls and wake up
    
    - Forward port code from cm-11.0 and adjust for 5.0
    - Fix not being able to adjust volume when music control is on
    - Disable screen off volume/music control when wake key is enabled
    
    Change-Id: Ie0ad83965c41fd33120490c25a4ff3d9b2cbeb37
    
    Keep quiet when volume keys are used to wake up device
    
    - Userspace will make a 'beep' with it receives a key up, so
      consume that event as well.
    - Removed wake key check in music control code as it will already
      be disabled here.
    
    Change-Id: I93839acd39aec8a2ee40291f833a31f6b048c9f8
    
    PhoneWindowManager: Allow track skip during ambient display
    
    Change-Id: I2463579e056364652b549524bc9775da4fa35b1f
    
    PhoneWindowManager: add LineageButtons volumekey hook
    
    We can't use a generic DeviceKeyHandler here, because we don't want to
    override some of the AOSP logic (silencing incoming calls, screenshots)
    and we need to know if the device is interactive or not (for screen-off
    music controls)
    
    Change-Id: I485c2f6006c5bbd358ba0cbd32917689826c5c6d

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -254,6 +254,8 @@ import com.android.server.wm.DisplayRotation;
 import com.android.server.wm.WindowManagerInternal;
 import com.android.server.wm.WindowManagerInternal.AppTransitionListener;
 
+import com.evervolv.internal.buttons.ButtonManager;
+
 import dalvik.system.PathClassLoader;
 
 import java.io.File;
@@ -748,6 +750,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             ));
 
     private final List<DeviceKeyHandler> mDeviceKeyHandlers = new ArrayList<>();
+
+    private ButtonManager mButtonManager;
 
     private static final int MSG_DISPATCH_MEDIA_KEY_WITH_WAKE_LOCK = 3;
     private static final int MSG_DISPATCH_MEDIA_KEY_REPEAT_WITH_WAKE_LOCK = 4;
@@ -3370,6 +3374,18 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
+    private static boolean isDeviceDreaming() {
+        IDreamManager dreamManager = getDreamManager();
+        if (dreamManager != null) {
+            try {
+                return dreamManager.isDreaming();
+            } catch (RemoteException e) {
+                // fine, stay asleep then
+            }
+        }
+        return false;
+    }
+
     static IDreamManager getDreamManager() {
         return IDreamManager.Stub.asInterface(
                 ServiceManager.checkService(DreamService.DREAM_SERVICE));
@@ -5201,6 +5217,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     // {@link interceptKeyBeforeDispatching()}.
                     result |= ACTION_PASS_TO_USER;
                 } else if ((result & ACTION_PASS_TO_USER) == 0) {
+                    if (mButtonManager.handleVolumeKey(event, interactive)) {
+                        break;
+                    }
+
                     // If we aren't passing to the user and no one else
                     // handled it send it to the session manager to
                     // figure out.
@@ -5684,6 +5704,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return false;
         }
 
+        if ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN || keyCode == KeyEvent.KEYCODE_VOLUME_UP)
+                && isDeviceDreaming()) {
+            return false;
+        }
+
         // Send events to keyguard while the screen is on and it's showing.
         if (isKeyguardShowingAndNotOccluded()) {
             return true;
@@ -5693,14 +5718,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (isDefaultDisplay) {
             // Send events to a dozing dream since the dream is in control of the state of the
             // screen.
-            IDreamManager dreamManager = getDreamManager();
-
-            try {
-                if (dreamManager != null && dreamManager.isDreaming()) {
-                    return true;
-                }
-            } catch (RemoteException e) {
-                Slog.e(TAG, "RemoteException when checking if dreaming", e);
+            if (isDeviceDreaming()) {
+                return true;
             }
         }
 
@@ -6428,6 +6447,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mKeyguardDelegate.onBootCompleted();
             }
         }
+
+        mButtonManager = new ButtonManager(mContext);
 
         mAutofillManagerInternal = LocalServices.getService(AutofillManagerInternal.class);
         mGestureLauncherService = LocalServices.getService(GestureLauncherService.class);
