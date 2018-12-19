ac7b288e501764a332127f163f5133991d2394ac
a.derendyaev <a.derendyaev@magdv.com>
2018-12-19 21:57:45 +0800

    SystemUI: runtime configurable audio panel location
    Co-authored-by: Alex Cruz <alex@dirtyunicorns.com>
    Co-authored-by: Arian <arian.kulmer@web.de>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: programminghoch10 <hoch10@kabelbw.de>
    Co-authored-by: Sam Mortimer <sam@mortimer.me.uk>
    Change-Id: If22c97eb1ce4cfb13396d182d90786f4b7acaee7

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -55,6 +55,7 @@ import android.content.res.ColorStateList;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.database.ContentObserver;
 import android.graphics.Color;
 import android.graphics.Outline;
 import android.graphics.PixelFormat;
@@ -146,6 +147,8 @@ import com.android.systemui.volume.domain.interactor.VolumePanelNavigationIntera
 import com.android.systemui.volume.panel.shared.flag.VolumePanelFlag;
 import com.android.systemui.volume.ui.navigation.VolumeNavigator;
 import com.google.android.msdl.domain.MSDLPlayer;
+
+import evervolv.provider.EVSettings;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -301,6 +304,9 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     private ViewStub mODICaptionsTooltipViewStub;
     @VisibleForTesting View mODICaptionsTooltipView = null;
 
+    // Volume panel placement left or right
+    private boolean mVolumePanelOnLeft;
+
     private final boolean mUseBackgroundBlur;
     private Consumer<Boolean> mCrossWindowBlurEnabledListener;
     private BackgroundBlurDrawable mDialogRowsViewBackground;
@@ -403,6 +409,25 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                         enabled ? dialogRowsViewColorAboveBlur : dialogRowsViewColorNoBlur);
                 mDialogRowsView.invalidate();
             };
+        }
+
+        if (!mIsTv) {
+            ContentObserver volumePanelOnLeftObserver = new ContentObserver(null) {
+                @Override
+                public void onChange(boolean selfChange) {
+                    final boolean volumePanelOnLeft = EVSettings.Secure.getInt(
+                            mContext.getContentResolver(),
+                            EVSettings.Secure.VOLUME_PANEL_ON_LEFT, 0) != 0;
+                    if (mVolumePanelOnLeft != volumePanelOnLeft) {
+                        mVolumePanelOnLeft = volumePanelOnLeft;
+                        mHandler.post(mControllerCallbackH::onConfigurationChanged);
+                    }
+                }
+            };
+            mContext.getContentResolver().registerContentObserver(
+                    EVSettings.Secure.getUriFor(EVSettings.Secure.VOLUME_PANEL_ON_LEFT),
+                    false, volumePanelOnLeftObserver);
+            volumePanelOnLeftObserver.onChange(true);
         }
 
         initDimens();
@@ -573,6 +598,12 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         lp.windowAnimations = -1;
 
         mOriginalGravity = mContext.getResources().getInteger(R.integer.volume_dialog_gravity);
+        if (!mIsTv) {
+            // Clear the pre-defined gravity for left or right,
+            // this is handled by mVolumePanelOnLeft
+            mOriginalGravity &= ~(Gravity.LEFT | Gravity.RIGHT);
+            mOriginalGravity |= mVolumePanelOnLeft ? Gravity.LEFT : Gravity.RIGHT;
+        }
         mWindowGravity = Gravity.getAbsoluteGravity(mOriginalGravity,
                 mContext.getResources().getConfiguration().getLayoutDirection());
         lp.gravity = mWindowGravity;
