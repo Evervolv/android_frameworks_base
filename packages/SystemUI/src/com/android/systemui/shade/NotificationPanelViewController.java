068fbfa86d8864bb070f1599a35eb7d606504a98
jhonboy121 <alfredmathew05@gmail.com>
2022-01-09 14:29:01 +0530

    SystemUI: Add edge light customizations [1/2]
    * Drawables and layout is from omni's commit
    * This a completely rewritten (in kt) and a non-hacky version of their work
    
    * (Stallix) - Adapt for A13 QPR1
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: I9d51ea356b56a68feb62c303a60508809ceb7ed5
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -343,6 +343,7 @@ public final class NotificationPanelViewController implements
     private boolean mAnimateNextPositionUpdate;
     private final ScreenOffAnimationController mScreenOffAnimationController;
     private final UnlockedScreenOffAnimationController mUnlockedScreenOffAnimationController;
+    private final EdgeLightViewController mEdgeLightViewController;
     private TrackingStartedListener mTrackingStartedListener;
     private OpenCloseListener mOpenCloseListener;
     private GestureRecorder mGestureRecorder;
@@ -642,7 +643,8 @@ public final class NotificationPanelViewController implements
             MSDLPlayer msdlPlayer,
             BrightnessMirrorShowingRepository brightnessMirrorShowingRepository,
             BlurConfig blurConfig,
-            Lazy<ShadeDisplaysRepository> shadeDisplaysRepository) {
+            Lazy<ShadeDisplaysRepository> shadeDisplaysRepository,
+            EdgeLightViewController edgeLightViewController) {
         mBlurConfig = blurConfig;
         SceneContainerFlag.assertInLegacyMode();
         keyguardStateController.addCallback(new KeyguardStateController.Callback() {
@@ -766,6 +768,7 @@ public final class NotificationPanelViewController implements
         mUnlockedScreenOffAnimationController = unlockedScreenOffAnimationController;
         mLastDownEvents = new NPVCDownEventState.Buffer(MAX_DOWN_EVENT_BUFFER_SIZE);
         mDeviceEntryFaceAuthInteractor = deviceEntryFaceAuthInteractor;
+        mEdgeLightViewController = edgeLightViewController;
 
         int currentMode = navigationModeController.addListener(
                 mode -> mIsGestureNavigation = QuickStepContract.isGesturalMode(mode));
@@ -871,6 +874,9 @@ public final class NotificationPanelViewController implements
                         .getKeyguardStatusBarViewController();
         mKeyguardStatusBarViewController.init();
         mNotificationContainerParent = mView.findViewById(R.id.notification_container_parent);
+
+        mEdgeLightViewController.setEdgeLightView(mView.findViewById(R.id.edge_light_container));
+
         mNotificationStackScrollLayoutController.setOnHeightChangedListener(
                 new NsslHeightChangedListener());
         mNotificationStackScrollLayoutController.setOnEmptySpaceClickListener(
